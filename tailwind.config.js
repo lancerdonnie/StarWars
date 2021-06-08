93565993d32b3203e9eea643430a0fdc06da48ac
lancerdonnie <jideadedejifirst@gmail.com>
2021-06-08 14:27:08 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          DEFAULT: 'black',
+        },
+        alt: {
+          DEFAULT: 'yellow',
+          2: '#5c1ed6',
+        },
+      },
+      screens: {
+        sm: { max: '640px' },
+        md: { max: '768px' },
+        lg: { max: '1024px' },
+        xl: { max: '1280px' },
+        '2xl': { max: '1536px' },
+      },
+      fontFamily: {
+        orb: ['orbitron'],
+        mont: ['Montserrat'],
+        space: ['Space Mono'],
+      },
+      // fontSize: {
+      //   md: '14px',
+      // },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
